744730d2278ab48f592cf40b79376cb12ea9f080
Aryan Agrawal <aryanagrawal294@gmail.com>
2024-05-31 23:10:05 +0530

    conversion operation cont....

---
+++ .history/01_basics/03_conversionOperation_20240531151307.js
@@ -0,0 +1,25 @@
+let score = "Aryan";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = "aryan";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true;  0 => false
+// "" => false
+// "aryan" => true
+
+
+let someNumber = 21;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
