7c759139a4b9099439f031c06086fda714aca057
Aryan Agrawal <aryanagrawal294@gmail.com>
2024-08-16 15:38:40 +0530

    "Commented out existing console logs, added new property and method to JsUser object, and added a new console log to print the greeting method."

---
+++ .history/02_basics/03_objects_20240815181642.js
@@ -0,0 +1,37 @@
+// singleton
+
+// object literals
+
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+  name: "Aryan",
+  "full name": "Aryan Agrawal",
+  age: 18,
+  [mySym]: "key1",
+  location: "Mumbai",
+  hobbies: ["Coding", "Reading", "Playing"],
+  email: "aryanagrawal00@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Friday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+
+
+JsUser.email = "aryanagrawal@chatgpt.com"
+Object.freeze(JsUser)
+JsUser.email = "aryanagrawal@shadcn.com"
+console.log(JsUser);
+
+
+JsUser.greeting = function() {
+  console.log("Hello, I am Aryan");
+}
+
+console
